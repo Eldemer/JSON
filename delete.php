30d930d60ad3a39303a9d92597af52d1e9ee9d05
ahmedeldemer <ahmedeldemer88@gmail.com>
2024-08-03 16:36:12 +0300

    HH

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ include 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['Id'];
 
-    $sql = "DELETE FROM crud_table WHERE id=$id";
+    $sql = "DELETE FROM crud_table WHERE Id=$id";
     if ($conn->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
